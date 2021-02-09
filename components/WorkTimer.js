cc98db8962a1c8633a51f42ee6ce46708e93f277
yenasamuel <78803322+yenasamuel@users.noreply.github.com>
2021-02-09 21:49:43 +0100

    WorkTimer: design of list of parts

--- components/WorkTimer.js
+++ components/WorkTimer.js
@@ -2,9 +2,9 @@
 DevD.component("work-timer", {
   template:
     /*html*/
-    `<div class="w-80 border-solid border-2 border-blue-600 m-1 p-2 rounded-lg">
+    `<div class="w- border-solid border-2 border-blue-600 m-1 p-2 rounded-lg">
     <widget-header name="Work Timer" version="v0.1"></widget-header>
-    <div class="flex w-full">
+    <div class="divtop flex w-full">
       <div class="flex-1">
         <comp-button :disabled="startBtnDisabled" name="Start" eventname="start-click" @start-click="startCounter"
           link=""></comp-button><br>
@@ -28,6 +28,25 @@ DevD.component("work-timer", {
         </div>
       </div>
     </div>
+    <div class="divbottom flex w-full pt-3">
+      <div class="flex-1">
+        <h4 class="text-lg">Total work time: {{ totalTime }}</h4>
+        <h4 class="text-lg">Total break time: {{ totalTime }}</h4>
+        <input type="text" placeholder="Done part name"
+          class="rounded px-1 bg-blue-100 border-solid border border-blue-400 focus:bg-blue-200 focus:border-2 focus:border-solid focus:border-blue-500">
+        <comp-button name="New part" eventname="newpart-click" @newpart-click="newPart" link=""></comp-button>
+      </div>
+      <div class="w-80 pl-3">
+        <div class="break-words word border border-blue-200 w-full px-2">
+          <h6 class="text-xl">Saved parts</h6>
+          <div>
+            <ol>
+              <li class="hover:bg-yellow-400 px-1" v-for="part in parts">{{ part.number + ". " + part.name + " - " + part.humanTime}}</li>
+            </ol>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>`,
   data() {
     return {
@@ -37,6 +56,11 @@ DevD.component("work-timer", {
       totalBreakTime: 0,
       startBtnDisabled: false,
       breakBtnDisabled: true,
+      parts: [
+        { number: 2, name: "partie cool", humanTime: "3 min" },
+        { number: 2, name: "partie cool", humanTime: "3 min" },
+        { number: 2, name: "partie cool", humanTime: "3 min" },
+      ],
     };
   },
   methods: {
