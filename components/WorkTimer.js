d2483de462f349ffb70ddb9f9ef05c4600b3cfab
Samuel Roland <47849646+samuelroland@users.noreply.github.com>
2021-02-11 23:24:31 +0100

    WorkTimer: big refactor of data vars naming, and time management
    new computed properties and methods for dates and time management. storage of start and end for each part...

--- components/WorkTimer.js
+++ components/WorkTimer.js
@@ -7,9 +7,9 @@ DevD.component("work-timer", {
     <div class="flex">
     <div class="w-full divtop">
       <div class="inline-block">
-        <comp-button :disabled="startBtnDisabled" name="Start" eventname="start-click" @start-click="startCounter"
+        <comp-button :disabled="startBtnDisabled" name="Work Timer ON" eventname="start-click" @start-click="workCounter"
           link=""></comp-button>
-          <comp-button :disabled="breakBtnDisabled" name="Pause" eventname="break-click" @break-click="breakCounter"
+          <comp-button :disabled="breakBtnDisabled" name="Break Timer ON" eventname="break-click" @break-click="breakCounter"
             link=""></comp-button>
       </div>
       <div>
@@ -17,22 +17,19 @@ DevD.component("work-timer", {
           <span class="mr-2">Work Timer</span>
           <input type="text"
             class="w-16 px-1 bg-blue-100 border border-blue-400 border-solid rounded focus:bg-blue-200 focus:border-2 focus:border-solid focus:border-blue-500"
-            v-model="currentWorkTime" disabled>
+            v-model="currentWorkTimer" disabled>
         </div>
         <div class="flex justify-end pt-1">
           <span class="mr-2">Break Timer</span>
           <input type="text"
             class="w-16 px-1 bg-blue-100 border border-blue-400 border-solid rounded focus:bg-blue-200 focus:border-2 focus:border-solid focus:border-blue-500"
-            v-model="currentBreakTime" disabled>
+            v-model="currentBreakTimer" disabled>
         </div>
       </div>
     
       <div class="">
-        <h4 class="text-lg">Total work time: {{ totalTime }}</h4>
-        <h4 class="text-lg">Total break time: {{ totalTime }}</h4>
-        <input type="text" placeholder="Done part name"
-          class="px-1 bg-blue-100 border border-blue-400 border-solid rounded focus:bg-blue-200 focus:border-2 focus:border-solid focus:border-blue-500">
-        <br>
+        <h4 class="text-lg">Total work time: {{ totalWorkTime }}</h4>
+        <h4 class="text-lg">Total break time: {{ totalBreakTime }}</h4>
         <div class="pt-1">
         <comp-button name="New part" eventname="newpart-click" @newpart-click="newPart" link=""></comp-button>
         </div>
@@ -44,10 +41,13 @@ DevD.component("work-timer", {
           <h6 class="text-xl">Saved parts</h6>
           <div>
             <ol>
-              <li class="pl-1 cursor-pointer hover:bg-yellow-400" v-for="part in parts">{{ part.number + ". " + this.fulldate(part.start) + " - " + part.humanTime}} <img src="icons/checked.png" class="inline w-4" v-if="part.validated" /></li>
+              <li class="pl-1 cursor-pointer hover:bg-yellow-400" v-for="part in parts">{{ part.number + ". " + this.timeInHourMinutes(part.start) + "-" + this.timeInHourMinutes(part.end) + " - " + diffTimeInHours(part.start, part.end)}} 
+              <img src="icons/checkbox.svg" class="inline w-4" v-if="part.validated" />
+              <img src="icons/checked.png" class="inline w-4" v-if="!part.validated" />
+              </li>
             </ol>
           </div>
-          <div>
+          <div :class="{invisible: selectedPart == null}">
           <hr class="my-1 border border-blue-400">
           Choose a type: <br>
           <select name="activitytype">
@@ -65,9 +65,9 @@ DevD.component("work-timer", {
           <option value="12">Cours Matu</option>
           <option value="13">Test</option></select>
           <br>Converted time:<br>
-          <input class="w-10 px-1" type="text" name="duration" step="0.5" max="10" min="0.5" value="4.5" disabled v-model="currentPartConvertedTime"><br>
-          <textarea class="w-full p-1 mt-1" placeholder="Describe what was the work in this part...">{{ currentPartText }}</textarea>
-          <comp-button name="Save here" eventname="" link="asdf">Save here</comp-button>
+          <input class="w-10 px-1" type="text" name="duration" step="0.5" max="10" min="0.5" value="4.5" disabled v-model="selectedPartConvertedTimeIfNotNull"><br>
+          <textarea class="w-full p-1 mt-1" placeholder="Describe what was the work in this part...">{{ selectedPartTextIfNotNull }}</textarea>
+          <comp-button name="Save here" eventname="" link=" ">Save here</comp-button>
           </div>
         </div>
       </div>
@@ -76,12 +76,12 @@ DevD.component("work-timer", {
   </div>`,
   data() {
     return {
-      currentWorkTime: 0,
+      //Timers for work and break times
+      currentWorkTimer: 0,
+      currentBreakTimer: 0,
+      selectedPart: null,
       currentPartConvertedTime: 0,
       currentPartType: 0,
-      currentBreakTime: 0,
-      totalTime: 0,
-      totalBreakTime: 0,
       startBtnDisabled: false,
       breakBtnDisabled: true,
       parts: [
@@ -89,31 +89,40 @@ DevD.component("work-timer", {
           number: 1,
           validated: true,
           text: "test texte",
-          start: new Date("2020-03-04"),
-          end: Date("2020-02-02"),
+          start: new Date("2020-03-04 12:03:02"),
+          end: new Date("2020-03-04 12:30:02"),
+          humanTime: "156 min",
+        },
+        {
+          number: 1,
+          validated: false,
+          text: "test texte",
+          start: new Date("2020-03-04 08:32:02"),
+          end: new Date("2020-03-04 12:20:02"),
           humanTime: "156 min",
         },
         {
           number: 1,
           validated: true,
           text: "test texte",
-          start: new Date("2020-03-04"),
-          end: Date("2020-02-02"),
-          humanTime: "156 min",
-        },
-        {
-          number: 1,
-          validated: true,
-          text: "test texte",
-          start: new Date("2020-03-04"),
-          end: Date("2020-02-02"),
+          start: new Date("2020-03-04 08:08:02"),
+          end: new Date("2020-03-04 14:25:02"),
           humanTime: "156 min",
         },
       ],
     };
   },
   methods: {
-    startCounter() {},
+    workCounter() {
+      setInterval(() => {
+        this.currentWorkTime++;
+      }, 1000);
+    },
+    breakCounter() {
+      setInterval(() => {
+        this.currentBreakTime++;
+      }, 1000);
+    },
     fulldate(date) {
       return (
         date.getDate() +
@@ -123,14 +132,70 @@ DevD.component("work-timer", {
         date.getFullYear()
       );
     },
+    timeInHourMinutes(date) {
+      nbHours = date.getHours();
+      if (nbHours < 10) {
+        nbHours = "0" + nbHours;
+      }
+      nbMinutes = date.getMinutes();
+      if (nbMinutes < 10) {
+        nbMinutes = "0" + nbMinutes;
+      }
+      return nbHours + ":" + nbMinutes;
+    },
+    diffTimeInHours(start, end, humanValue = false) {
+      diff = end - start;
+      console.log(start);
+      console.log(end);
+      console.log(diff);
+      diffInDate = new Date(diff);
+      nbHours =
+        diffInDate.getHours() - 1 + "." + diffInDate.getMinutes().toFixed(0);
+      if (humanValue == true) {
+        nbHours += "h";
+      }
+      return nbHours;
+    },
   },
   computed: {
-    currentPartConvertedTime() {
-      if (true) {
-        //if a part is displayed
+    selectedPartConvertedTimeIfNotNull() {
+      if (this.currentPartConvertedTime != null) {
         return this.currentPartConvertedTime.time; //the time rounded to the half point under.
       }
       return 0;
+    },
+    totalWorkTime() {
+      total = 0;
+      Array.prototype.forEach.call(this.parts, function (part) {
+        //set vars
+        start = part.start;
+        end = part.end;
+        humanValue = true;
+
+        //copy paste temp
+        diff = end - start;
+        console.log(start);
+        console.log(end);
+        console.log(diff);
+        diffInDate = new Date(diff);
+        nbHours =
+          diffInDate.getHours() - 1 + "." + diffInDate.getMinutes().toFixed(0);
+        total += parseFloat(nbHours);
+        console.log(total);
+      });
+      if (humanValue == true) {
+        total += "h";
+      }
+      return total; //calculate the total work time with all parts
+    },
+    totalBreakTime() {
+      return "20min"; //calculate the total break time with all parts
+    },
+    selectedPartTextIfNotNull() {
+      if (this.selectedPart != null) {
+        return this.selectedPart.text;
+      }
+      return "";
     },
   },
 });
